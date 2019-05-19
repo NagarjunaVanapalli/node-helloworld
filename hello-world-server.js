92a770469804d2904f0648979fd9d1ec78e05a94
Nagarjuna Vanapalli <nagarjuna.dj@gmail.com>
2019-05-19 16:35:17 +0530

    #initial Checking

---
+++ hello-world-server.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function handler(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(4825, 'localhost');
